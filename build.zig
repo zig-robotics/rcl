383a5c1f60d976ec04028af5481f8165e0b054a2
zig-robotics <141579568+zig-robotics@users.noreply.github.com>
2024-11-16 13:09:49 -0600

    cleanup, pic, visibility hidden

--- build.zig
+++ build.zig
@@ -1,24 +1,15 @@
 const std = @import("std");
 
-// TODO move this somewhere generic?
-pub fn linkLibraryRecursive(src: *std.Build.Step.Compile, dependency: *std.Build.Step.Compile) void {
-    src.linkLibrary(dependency);
-    for (dependency.step.dependencies.items) |dep| switch (dep.id) {
-        .compile => {
-            const compile: *std.Build.Step.Compile = @fieldParentPtr("step", dep);
-            src.linkLibrary(compile);
-            linkLibraryRecursive(src, compile);
-        },
-        else => {},
-    };
-}
-
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const linkage = b.option(std.builtin.LinkMode, "linkage", "Specify static or dynamic linkage") orelse .dynamic;
+    const linkage = b.option(
+        std.builtin.LinkMode,
+        "linkage",
+        "Specify static or dynamic linkage",
+    ) orelse .dynamic;
     const upstream = b.dependency("rcl", .{});
 
     const rcutils_dep = b.dependency("rcutils", .{
@@ -43,6 +34,7 @@ pub fn build(b: *std.Build) void {
         .root_module = .{
             .target = target,
             .optimize = optimize,
+            .pic = if (linkage == .dynamic) true else null,
         },
         .name = "rcl_yaml_param_parser",
         .kind = .lib,
@@ -70,16 +62,26 @@ pub fn build(b: *std.Build) void {
     yaml_param_parser.linkLibC();
 
     yaml_param_parser.addIncludePath(upstream.path("rcl_yaml_param_parser/include"));
-    yaml_param_parser.installHeadersDirectory(upstream.path("rcl_yaml_param_parser/include"), "", .{});
+    yaml_param_parser.installHeadersDirectory(
+        upstream.path("rcl_yaml_param_parser/include"),
+        "",
+        .{},
+    );
 
-    yaml_param_parser.addCSourceFiles(.{ .root = upstream.path("rcl_yaml_param_parser"), .files = &.{
-        "src/add_to_arrays.c",
-        "src/namespace.c",
-        "src/node_params.c",
-        "src/parse.c",
-        "src/parser.c",
-        "src/yaml_variant.c",
-    } });
+    yaml_param_parser.addCSourceFiles(.{
+        .root = upstream.path("rcl_yaml_param_parser"),
+        .files = &.{
+            "src/add_to_arrays.c",
+            "src/namespace.c",
+            "src/node_params.c",
+            "src/parse.c",
+            "src/parser.c",
+            "src/yaml_variant.c",
+        },
+        .flags = &.{
+            "-fvisibility=hidden",
+        },
+    });
     yaml_param_parser.linkLibrary(rmw_dep.artifact("rmw"));
 
     yaml_param_parser.linkLibrary(libyaml_dep.artifact("yaml"));
@@ -90,13 +92,16 @@ pub fn build(b: *std.Build) void {
         .root_module = .{
             .target = target,
             .optimize = optimize,
+            .pic = if (linkage == .dynamic) true else null,
         },
         .name = "rcl",
         .kind = .lib,
         .linkage = linkage,
     });
 
-    rcl.addIncludePath(b.dependency("ros2_tracing", .{}).namedWriteFiles("tracetools").getDirectory());
+    rcl.addIncludePath(b.dependency("ros2_tracing", .{}).namedWriteFiles(
+        "tracetools",
+    ).getDirectory());
 
     rcl.addIncludePath(upstream.path("rcl/include"));
     rcl.addIncludePath(upstream.path("rcl/src"));
@@ -108,14 +113,23 @@ pub fn build(b: *std.Build) void {
     rcl.linkLibrary(rosidl_dep.artifact("rosidl_dynamic_typesupport"));
     rcl.addIncludePath(rosidl_dep.namedWriteFiles("rosidl_typesupport_interface").getDirectory());
 
-    linkLibraryRecursive(rcl, yaml_param_parser);
+    rcl.linkLibrary(yaml_param_parser);
+    rcl.linkLibrary(libyaml_dep.artifact("yaml"));
     rcl.linkLibrary(rcl_logging_dep.artifact("rcl_logging_interface"));
 
     // TODO rosidl should probably have a helper script for linking the correct things?
-    linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("type_description_interfaces__rosidl_generator_c"));
-    linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("type_description_interfaces__rosidl_typesupport_c"));
-    linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("rcl_interfaces__rosidl_generator_c"));
-    linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("rcl_interfaces__rosidl_typesupport_c"));
+    // linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("type_description_interfaces__rosidl_generator_c"));
+    // linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("type_description_interfaces__rosidl_typesupport_c"));
+    // linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("rcl_interfaces__rosidl_generator_c"));
+    // linkLibraryRecursive(rcl, rcl_interfaces_dep.artifact("rcl_interfaces__rosidl_typesupport_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("type_description_interfaces__rosidl_generator_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("type_description_interfaces__rosidl_typesupport_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("service_msgs__rosidl_generator_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("service_msgs__rosidl_typesupport_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("builtin_interfaces__rosidl_generator_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("builtin_interfaces__rosidl_typesupport_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("rcl_interfaces__rosidl_generator_c"));
+    rcl.linkLibrary(rcl_interfaces_dep.artifact("rcl_interfaces__rosidl_typesupport_c"));
 
     rcl.addCSourceFiles(.{
         .root = upstream.path("rcl/src/rcl"),
@@ -161,6 +175,7 @@ pub fn build(b: *std.Build) void {
         },
         .flags = &.{
             "-DROS_PACKAGE_NAME=\"rcl\"", // TODO this is used for logging in many packages, normally comes from ament, see if there's a way to automate this https://github.com/ros2/ament_cmake_ros/blob/jazzy/ament_cmake_ros/ament_cmake_ros-extras.cmake.in#L25
+            "-fvisibility=hidden",
         },
     });
     b.installArtifact(rcl);
